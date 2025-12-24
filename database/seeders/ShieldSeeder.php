71a719ee867eeb9cc3645c2d85b9f244915da363
muharief12 <muhammadarief379@gmail.com>
2025-12-24 14:19:31 +0700

    Fixing RBAC configuration

---
+++ database/seeders/ShieldSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class ShieldSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::firstOrCreate([
+            'name' => 'super_admin',
+            'guard_name' => 'web',
+        ]);
+    }
+}
